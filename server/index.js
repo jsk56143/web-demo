928af76fa21ee0a2794e15b0342bafb5a443231a
jsk56143 <jsk56143@uga.edu>
2022-09-07 03:11:42 -0400

    Added first route

--- server/index.js
+++ server/index.js
@@ -3,13 +3,17 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://<user>:<password>@cluster0.jaak6fa.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://mongobee:mongobee123@cluster0.jaak6fa.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
