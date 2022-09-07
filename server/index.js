414a671772cf4237118804e06f3d7aa2a6633f50
jsk56143 <jsk56143@uga.edu>
2022-09-07 02:46:23 -0400

    set up mongodb

--- server/index.js
+++ server/index.js
@@ -8,3 +8,10 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://<user>:<password>@cluster0.jaak6fa.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
